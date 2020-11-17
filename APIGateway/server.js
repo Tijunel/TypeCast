be6ee4d58a1fd85112a90152c551cdb4ec113f0f
Justin Tijunelis <35385306+Tijunel@users.noreply.github.com>
2020-11-17 15:30:36 -0700

    Added project folders and microservices

---
+++ APIGateway/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+    next();
+});
+
+// Configure the bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Configure the CORs middleware
+app.use(cors());
+
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.join(__dirname, '../Frontend/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../Frontend/build', 'index.html'));
+    });
+};
+
+// Catch any bad requests
+app.get('*', (req, res) => {
+    res.status(200).json({
+        msg: 'Catch All'
+    });
+});
+
+// Configure our server to listen on the port defiend by our port variable
+app.listen(port, () => console.log(`Server running on port ${port}...`));
