3e5be3a977876f4ce05ffa02c25c76b540ba3b3b
Justin Tijunelis <35385306+Tijunel@users.noreply.github.com>
2020-11-18 16:40:53 -0700

    Created top navigation bar

--- Frontend/src/components/navigation/topNav.js
+++ Frontend/src/components/navigation/topNav.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { Nav, Navbar, Button } from 'react-bootstrap';
+import './_styling/topNav.css';
 
 export default class TopNav extends React.Component {
     constructor() {
         super();
     }
 
+    handleProfile = () => {
+        // Go to profile if signed in
+        // Else go to 
+    }
+
     render = () => {
         return (
-            <div id='top-nav'>
-
-            </div>
+            <header>
+                <Navbar id='top' collapseOnSelect expand='sm'fixed='top'>
+                    <Navbar.Brand className='link-0' href='/'><b>TypeCast</b></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className='mr-auto'>
+                            <Nav.Link className="link-1" href="/">Home</Nav.Link> 
+                            <Nav.Link className="link-2" href="/type">Type</Nav.Link> 
+                            <Nav.Link className="link-3" href="/join">Game</Nav.Link>
+                        </Nav>
+                        <Nav className='log'>
+                            {/* Make this dynamic (Sign In vs. Sign Out) */}
+                            <Nav.Link className="link-4" href="/signin">Sign In/Out</Nav.Link>
+                            <Nav.Link className="link-5" href="/profile">Profile</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </header>
         );
     }
 }
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -9,6 +9,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Page imports
 import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
+import Join from './pages/join';
+import Lobby from './pages/lobby';
+import Profile from './pages/profile';
+import Game from './pages/game';
+import Error from './pages/error';
+
+import TopNav from './components/navigation/topNav';
 
 class App extends React.Component {
   constructor() {
@@ -22,12 +31,18 @@ class App extends React.Component {
   render = () => {
     return (
       <React.Fragment>
+        <TopNav/>
         <Router>
           <Switch>
-            <Route exact path="/" component={() => <Home/>}/>
-            <Route exact path="/home" component={() => <Home/>}/>
-            <Route exact path="/login" component={() => <Home/>}/>
-            <Route exact path="/register" component={() => <Home/>}/>
+            <Route path="/" component={() => <Home/>}/>
+            <Route path="/home" component={() => <Home/>}/>
+            <Route path="/login" component={() => <Login/>}/>
+            <Route path="/register" component={() => <Register/>}/>
+            <Route path="/join" component={() => <Join/>}/>
+            <Route path="/lobby/:id" component={() => <Lobby/>}/>
+            <Route path="/profile/:id" component={() => <Profile/>}/>
+            <Route path="/game/:id" component={() => <Game/>}/>
+            <Route path="*" component={() => <Error/>}/>
           </Switch>
         </Router>
       </React.Fragment>
