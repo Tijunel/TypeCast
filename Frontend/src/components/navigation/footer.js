df0bc5e653c537cc2d7cfef3cd635add6ba01e2b
Justin Tijunelis <35385306+Tijunel@users.noreply.github.com>
2020-11-19 13:32:51 -0700

    Minor updates to footer

--- Frontend/src/components/navigation/footer.js
+++ Frontend/src/components/navigation/footer.js
@@ -7,13 +7,21 @@ export default class Footer extends React.Component {
         super();
     }
 
+    handleAttributions = () => {
+        // Use react-router to take the user to /attributions
+    }
+
+    handleLicenses = () => {
+        // Use react-router to take the user to /licenses
+    }
+
     render = () => {
         return (
             <footer id='footer'>
                 <div id='footer-content'>
                     <b>Copyright &copy; TypeCast 2020. </b>
-                    <b id='attributions-button'>Attributions</b>
-                    <b id='licenses-button'>Licenses</b>
+                    <b onClick={this.handleAttributions} id='attributions-button'>Attributions</b>
+                    <b onClick={this.handleLicenses} id='licenses-button'>Licenses</b>
                 </div>
             </footer>
         );
