db46c9f7c0a68ccec790b5831237fd20a4fd29d3
Tijunel <justintijunel@gmail.com>
2020-12-02 14:47:20 -0700

    Wrapped up skeleton

--- APIGateway/Routes/userData.js
+++ APIGateway/Routes/userData.js
@@ -13,14 +13,16 @@ const userData = express.Router();
 //          userID:
 //              games:
 //                  gameID:
-//                      placement: string ("x/y") where x > 0, x <= y and ymax = 6
+//                      placement: string ('x/y') where x > 0, x <= y and ymax = 6
 //                      typingSpeed: float
 //                      time: float (seconds)
 //                      date: UTC
 
 // Get all game information for specified user
-userData.get("/games", withAuth, async(req, res) => {
-
+userData.get('/games', withAuth, async(req, res) => {
+    const games = await api.call('/games/', 'GET', {});
+    if (games.status === 200) res.status(200).json(games.body).end();
+    else res.sendStatus(games.status).end();
 });
 
 // Add a new game (Trigger after finishing a game)
--- APIGateway/Utilities/MSCall.js
+++ APIGateway/Utilities/MSCall.js
@@ -4,33 +4,29 @@ const got = require('got');
 
 class MSCall {
     constructor() {
-        this.api = got.extend({
-            responseType: 'json'
-        });
+        this.api = got.extend({ responseType: 'json' });
     }
 
     setURLPrefix = (prefix) => {
-        this.api = this.api.extend({
-            prefixUrl: prefix
-        });
+        this.api = this.api.extend({ prefixUrl: prefix });
     }
 
-    call = async(path, method, options) => {
+    call = async (path, method, options) => {
         const searchParameters = options.searchParameters || {};
         const headers = options.headers || {};
-        const json = options.json || (method === "GET" ? undefined : {});
+        const json = options.json || (method === 'GET' ? undefined : {});
         let response;
         try {
-            response = await this.api(path, {
-                headers, 
-                searchParameters,
-                method,
-                json
+            response = await this.api(path, {   // path contains req.params
+                headers,                        // Translates to req.headers
+                searchParameters,               // Translates to req.query or what is after ? in URL
+                method,                         // Translates to GET, POST, PUT, or DELETE
+                json                            // Translates to req.body
             });
         } catch (error) {
             return { status: 500 }
         }
-        response = {status: response.statusCode, body: response.body };
+        response = { status: response.statusCode, body: response.body };
         return response;
     }
 }
