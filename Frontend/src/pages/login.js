60fa86feff945ed0b069a10b71f28abdaef2a86b
CodyNS <codynscontact@gmail.com>
2020-12-04 14:34:42 -0700

    login and register pages updated

--- Frontend/src/pages/login.js
+++ Frontend/src/pages/login.js
@@ -2,36 +2,70 @@
 
 import React from 'react';
 import './_styling/login.css';
-import Form from 'react-bootstrap';
 
 export default class Login extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
     }
+  }
 
-    render = () => {
-        return (
-            <div id='login'>
-                <h1>LOG IN</h1>
-                <form>
-                  <label>
-                    <div id='label'>Username</div>
-                    <input type="text" name="name" />
-                  </label>
-                </form>
-                <form>
-                  <label>
-                    <div id='label'>Password</div>
-                    <input type="text" name="name" />
-                  </label>
-                </form>
-                <form>
-                  <button class="button">Log in</button>
-                </form>
-                <form>
-                  <button class="button reg">Register</button>
-                </form>
-            </div>
-        );
-    }
+  forgotPassword = () => {
+    // todo
+    alert("todo: implement this forgotPassword() method");
+
+  }
+
+  loginHandler = (event) => {
+    // todo: connect to the users db and do the login stuff
+    alert("todo: implement this loginHandler() method")
+
+    event.preventDefault();  // prevent page reload on form submission, if you want
+  }
+
+  myChangeHandler = (event) => {
+    let name = event.target.name;
+    let val = event.target.value;
+    this.setState({[name]: val});
+  }
+
+  render = () => {
+    return (
+      <div id='login'>
+
+        <h1>Log In</h1>
+
+        <form onSubmit={this.loginHandler}>
+          <label>Username</label>
+          <input 
+            type="text" 
+            name="username" 
+            value={this.state.username}
+            onChange={this.myChangeHandler}
+          />
+
+          <label>Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            value={this.state.password}
+            onChange={this.myChangeHandler}
+          />
+
+          <input type='submit' value='Log in'/>
+        </form>
+
+        <div id="regbox">
+         <button onClick={()=>window.location.href="/register"}>Register</button>
+        </div>
+
+        <div id="forgotpw">
+          <a href=''onClick={()=>this.forgotPassword()}>Forgot your password?</a>
+        </div>
+
+      </div>
+    );
+  }
 }
