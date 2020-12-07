e1de33c59790d0e455a06f617df45e2b1210fba5
Nathan Godard <nathan.godard1@ucalgary.ca>
2020-12-06 17:13:06 -0700

    Added user and game routes (nothing tested, expect bugs)

--- UserDataMS/Routes/games.js
+++ UserDataMS/Routes/games.js
@@ -6,17 +6,47 @@ const firebase = require('../Config/firebase');
 
 // Post a new game upon completion
 games.post('/', (req, res) => {
-
+    try {
+        const userID = req.user.ID;
+        const newGameRef = firebase.database().ref('users/' + userID + "/games").push();
+        newGameRef.set({
+            placement: req.data.placement,
+            typingSpeed: req.data.typingSpeed,
+            time: req.data.time,
+            date: req.data.date
+        });
+        res.status(200);
+    } catch (e) {
+        res.status(500).send('Error saving game details').end();
+    }
 });
 
 // Get all games
 games.get('/', (req, res) => {
-
+    try {
+        const userID = req.user.ID;
+        const gamesRef = firebase.database().ref('users/' + userID + "/games");
+        gamesRef.on("value", function(snapshot) {
+            res.data.games = snapshot.val();
+        }, function () {
+            console.log("Failed to retrieve games");
+        });
+        res.status(200);
+    } catch (e) {
+        res.status(500).send('Error getting games!').end();
+    }
 });
 
 // Delete all games (If user wants to reset stats)
 games.delete('/', (req, res) => {
-
+    try {
+        const userID = req.user.ID;
+        const userRef = firebase.database().ref('users/' + userID);
+        userRef.set({games: null}); //TODO: Not sure if this is the right way to remove all games or not
+        res.status(200);
+    } catch (e) {
+        res.status(500).send('Error deleting games!').end();
+    }
 });
 
 module.exports = games;
--- UserDataMS/Routes/users.js
+++ UserDataMS/Routes/users.js
@@ -6,11 +6,26 @@ const firebase = require('../Config/firebase');
 
 // Create new user entry in firebase on registration
 users.post('/', (req, res) => {
-    
+    try {
+        console.log(req);
+        const newUserRef = firebase.database().ref('users').push();
+        res.user.userID = newUserRef.key;
+        res.status(200);
+    } catch (e) {
+        res.status(500).send('Error creating user!').end();
+    }
+
 });
 
 // Delete user when they delete their account
 users.delete('/', (req, res) => {
+    try {
+        const userID = req.user.userID; //TODO: Verify that this is the correct way to get the UserID
+        const userRef = firebase.database().ref('users/' + userID);
+        userRef.remove();
+    } catch (e) {
+        res.status(500).send('Error deleting user!').end();
+    }
 
 });
 
