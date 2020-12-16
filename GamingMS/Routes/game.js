441b54639acc808a58a2ebf48c1985a9cc334765
Tijunel <justintijunel@gmail.com>
2020-12-15 21:06:23 -0700

    Fixed

--- GamingMS/Routes/game.js
+++ GamingMS/Routes/game.js
@@ -7,7 +7,7 @@ const asyncRedis = require('../Config/redis')[1];
 const snippets = require('./snippets')[0];
 
 game.get('/:id', async (req, res) => {
-    var value = await asyncRedis.get(req.params.id+'-game').then(value => {
+    var value = await asyncRedis.get(req.params.id + '-game').then(value => {
         return JSON.parse(value);
     });
     if (value !== null) res.status(200).json(value).end();
@@ -25,32 +25,32 @@ game.post('/start', async (req, res) => {
         player.finished = false;
     }
     game.code = snippets[Math.floor(Math.random() * snippets.length)];
-    redis.set(game.lobbyCode+'-game', JSON.stringify(game));
+    redis.set(game.lobbyCode + '-game', JSON.stringify(game));
     res.status(200).end();
 });
 
 // Make sure everyone is in the game
 game.post('/ready', async (req, res) => {
-    var game = await asyncRedis.get(req.body.lobbyCode+'-game').then(value => {
+    var game = await asyncRedis.get(req.body.lobbyCode + '-game').then(value => {
         return JSON.parse(value);
     });
     for (let player of game.players)
         if (player.username === req.body.username)
             player.isReady = true;
-    redis.set(game.lobbyCode+'-game', JSON.stringify(game));
+    redis.set(game.lobbyCode + '-game', JSON.stringify(game));
     let readyLeft = game.players.length;
     for (let player of game.players)
-        if (player.isReady) 
+        if (player.isReady)
             readyLeft -= 1;
     res.status(200).json({ readyLeft: readyLeft }).end();
 });
 
 game.post('/update', async (req, res) => {
-    var game = await asyncRedis.get(req.body.lobbyCode+'-game').then(value => {
+    var game = await asyncRedis.get(req.body.lobbyCode + '-game').then(value => {
         return JSON.parse(value);
     });
     for (let player of game.players) {
-        if(player.username === req.body.username) {
+        if (player.username === req.body.username) {
             player.charsFin = req.body.charsFin;
             player.time = req.body.time;
         }
@@ -62,14 +62,23 @@ game.post('/update', async (req, res) => {
             charsFin: player.charsFin,
             time: player.time
         });
-    }   
-    redis.set(req.body.lobbyCode+'-game', JSON.stringify(game));
+    }
+    redis.set(req.body.lobbyCode + '-game', JSON.stringify(game));
     res.status(200).json(updates).end();
 });
 
 // End point for each player 
 game.post('/delete', (req, res) => {
-    redis.del(req.body.lobbyCode+'-game');
+    var game = await asyncRedis.get(req.body.lobbyCode + '-game').then(value => {
+        return JSON.parse(value);
+    });
+    for (let player of game.players)
+        if (player.username === req.body.username)
+            player.finished = true;
+    let deleteDB = true;
+    for (let player of game.players)
+        if (!player.finished) deleteDB = false;
+    if (deleteDB) redis.del(req.body.lobbyCode + '-game');
     res.status(200).end();
 });
 
