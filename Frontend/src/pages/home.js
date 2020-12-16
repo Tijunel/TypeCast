517bdc070cf6a05c4e6f26c4c03412617ad96958
CodyNS <codynscontact@gmail.com>
2020-12-16 08:54:21 -0700

    home page desktop + mobile style polished

--- Frontend/src/pages/home.js
+++ Frontend/src/pages/home.js
@@ -23,19 +23,21 @@ export default class Home extends React.Component {
 	render = () => {
 		return (
 			<div id='home'>
-				<h1>Welcome to TypeCast!</h1>
-				<p>
-					TypeCast was created to put programmer's typing speed to the test. This site enables you
-					to create a profile with which you can track your stats, race your friends or randoms, or just practice!
-					Typical type racing websites track your speed using wpm (words per minute). Obviously, this just
-					won't do for programming, so we use lpm (lines per minute) where each "line" is counted as 80 characters.
-					We hope you enjoy this unique type racing experience!
-					<br /><br />
-					-The TypeCasters
-				</p><br />
+                <div id="homeTopSection">
+                    <h1>Welcome to TypeCast!</h1>
+                    <div id="blurb">
+                        TypeCast was created to put programmer's typing speed to the test. This site enables you
+                        to create a profile with which you can track your stats, race your friends or randoms, or just practice!
+                        Typical type racing websites track your speed using wpm (words per minute). Obviously, this just
+                        won't do for programming, so we use lpm (lines per minute) where each "line" is counted as 80 characters.
+                        We hope you enjoy this unique type racing experience!
+                        <br /><br />
+                        -The TypeCasters
+                    </div>
+                </div>
 				<button onClick={() => this.hostGame()}>Host Game</button>
 				<button onClick={() => window.location.href = "/join"}>Join Game</button>
-				<button onClick={() => window.location.href = "/profile"}>Profile</button>
+				<button onClick={() => window.location.href = "/profile"}>View Profile</button>
 			</div>
 		);
 	}
